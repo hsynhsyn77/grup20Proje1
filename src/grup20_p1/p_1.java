5df4c15c76f3dbd0de2f1f364dce8ea852cb2600
Aykut <aykutkorkmaz_33@outlook.com>
2022-07-06 21:17:06 +0300

    aykut1

--- src/grup20_p1/p_1.java
+++ src/grup20_p1/p_1.java
@@ -3,5 +3,8 @@ package grup20_p1;
 public class p_1 {
     public static void main(String[] args) {
         System.out.println("hoşgeldiniz hayırlı olsun");
+
+        System.out.println("nasılsın hüseyin şirikçi");
+
     }
 }
